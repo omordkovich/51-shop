1da82e329c70a5a6b4fddfb3dd8fbbf6d1874e9a
omordkovich <o.mordkovich@hotmail.com>
2025-09-02 00:19:53 +0200

    "first commit"

---
+++ src/main/java/ait/shop/ShopCohort51Application.java
@@ -0,0 +1,13 @@
+package ait.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopCohort51Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopCohort51Application.class, args);
+    }
+
+}
---
+++ src/test/java/ait/shop/ShopCohort51ApplicationTests.java
@@ -0,0 +1,13 @@
+package ait.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopCohort51ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
